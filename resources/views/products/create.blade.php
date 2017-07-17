3ffb06ba8f4863398392bf8251ad5a17f922d685
Mijael Saban <mijaelsaban@gmail.com>
2017-07-17 20:54:33 -0300

    include de los navs en create y products/index

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <title>Crear Evento</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link id="pagestyle" rel="stylesheet" type="text/css" href="../css/style.css">
   </head>
   <body>
+  @include('partials/nav')
 
  <div class="container">
         <div class="row">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,10 @@
 	<title>Productos</title>
 </head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link id="pagestyle" rel="stylesheet" type="text/css" href="../css/style.css">
 <body>
+
+@include('partials/nav')
 	<h1>Todos los Productos</h1>
 
 @foreach ($products->Chunk(3) as $productChunk)
