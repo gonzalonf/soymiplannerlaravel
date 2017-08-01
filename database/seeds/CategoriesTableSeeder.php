fc7367835872edd6587579b545e6be66171996e9
gonza <gonzalonf@gmail.com>
2017-08-01 20:23:49 -0300

    fix? seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,44 +12,147 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->truncate();
-        DB::insert($this->categories());
-
-        // DB::table('categories')->insert([
-        //     [
-        //     'category_name' => 'Lugar',
-        //     'created_at' => date('Y-m-d h:i:s'),
-        //     'updated_at' => date('Y-m-d h:i:s'),
-        //     ],
-        // ]);
-
+        DB::table('categories')->insert([
+            [
+            'id' => 1,
+            'category_name' => 'Locación',
+            'subcategory_child_of_id' => NULL,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => 2,
+            'category_name' => 'Decoración',
+            'subcategory_child_of_id' => NULL,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => 3,
+            'category_name' => 'Catering',
+            'subcategory_child_of_id' => NULL,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => 4,
+            'category_name' => 'Servicios',
+            'subcategory_child_of_id' => NULL,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => 5,
+            'category_name' => 'Otros',
+            'subcategory_child_of_id' => NULL,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Salones',
+            'subcategory_child_of_id' => 1,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Quintas',
+            'subcategory_child_of_id' => 1,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Otras locaciones',
+            'subcategory_child_of_id' => 1,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Decoración y ambientación',
+            'subcategory_child_of_id' => 2,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Mobiliario',
+            'subcategory_child_of_id' => 2,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Otros',
+            'subcategory_child_of_id' => 2,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Catering general',
+            'subcategory_child_of_id' => 3,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Mesa dulce',
+            'subcategory_child_of_id' => 3,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Tortas de boda y cumpleaños',
+            'subcategory_child_of_id' => 3,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'DJs y sonido',
+            'subcategory_child_of_id' => 4,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Foto y video',
+            'subcategory_child_of_id' => 4,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Animación y shows',
+            'subcategory_child_of_id' => 4,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Cotillón',
+            'subcategory_child_of_id' => 5,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Vestidos y trajes',
+            'subcategory_child_of_id' => 5,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ],
+            [
+            'id' => NULL,
+            'category_name' => 'Ramos y souvenirs',
+            'subcategory_child_of_id' => 5,
+            'created_at' => date('Y-m-d h:i:s'),
+            'updated_at' => date('Y-m-d h:i:s'),
+            ]
+        ]);
     }
-    private function categories()
-    {
-        return "INSERT INTO categories (category_name,subcategory_child_of_id,created_at,updated_at)
-        VALUES
-        /*1*/('Locación',NULL,now(),now()),
-        /*2*/('Decoración',NULL,now(),now()),
-        /*3*/('Catering',NULL,now(),now()),
-        /*4*/('Servicios',NULL,now(),now()),
-        /*5*/('Otros',NULL,now(),now()),
-        -- subcaregorías...
-        ('Salones',1,now(),now()),
-        ('Quintas',1,now(),now()),
-        ('Otras locaciones',1,now(),now()),
-        ('Decoracion y ambientación',2,now(),now()),
-        ('Mobiliario',2,now(),now()),
-        ('Otros',2,now(),now()),
-        ('Catering general',3,now(),now()),
-        ('Mesa dulce',3,now(),now()),
-        ('Tortas de boda y cumpleaños',3,now(),now()),
-        ('DJs y sonido',4,now(),now()),
-        ('Foto y video',4,now(),now()),
-        ('Animacion y shows',4,now(),now()),
-        ('Cotillon',5,now(),now()),
-        ('Vestidos y trajes a medida',5,now(),now()),
-        ('Ramos y souvenirs',5,now(),now())
-        ;";
-
 }
-}
-
