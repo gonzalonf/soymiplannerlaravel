75900c1ea7aee70aab01d331ee022f22e41a65eb
Mir <mirnixx@gmail.com>
2017-08-06 06:00:30 -0300

    Limpieza de rutas web.php
    Limpieza de rutas web.php

--- routes/web.php
+++ routes/web.php
@@ -2,63 +2,53 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes /// Route::get('rutaDeFantasia', 'controlador@metodo');
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
-*/
-
- //  Route::get('ruta', 'controlador@metodo');
-
+|--------------------------------------------------------------------------
+| VISTAS PRINCIPALES
+|--------------------------------------------------------------------------*/
 Route::get('/', 'IndexController@index');
 Route::get('/partials', 'IndexController@cuantosSomos');
 Route::get('/auth', 'ValidacionAjaxController@validadorAjax');
-
 Route::get('faq', 'FaqController@index');
-
-/////////////agregado x make:auth///////////////
-
+/*--------------------------------------------------------------------------
+| AUTH
+|--------------------------------------------------------------------------*/
 Auth::routes();
-
-// Route::patch('profile', 'ProfileController@update')->middleware('auth');
-
-///////////////////////////////////////////////
-
+/*--------------------------------------------------------------------------
+| PROFILE
+|--------------------------------------------------------------------------*/
 Route::get('profile', 'ProfileController@index')->middleware('auth');
 Route::get('profile/products', 'ProfileController@products')->middleware('auth');
 Route::get('profile/sales', 'ProfileController@sales')->middleware('auth');
 Route::get('profile/{id}', 'ProfileController@show');
-// nota(gon): cambié ligeramente esto para meter la logica publica en el mismo controlador
-Route::resource('profile', 'ProfileController');
-
-
-
+// Route::patch('profile', 'ProfileController@update')->middleware('auth');
+Route::resource('profile', 'ProfileController');// gon: cambié esto p/meter "publicar" en el mismo controlador
+Route::get('/profile', 'ProfileController@showRegistrationForm2');
+/*--------------------------------------------------------------------------
+| PRODUCTS PUBLIC
+|--------------------------------------------------------------------------*/
 Route::get('/products', 'ProductsController@index');
 Route::get('/products/filter', 'ProductsController@filter');
 
-
 Route::get('/products/create', 'ProductsController@create');
 Route::get('/products/{id}', 'ProductsController@show');
-
-
+/*--------------------------------------------------------------------------
+| PRODUCTS PROFILE
+|--------------------------------------------------------------------------*/
 Route::get('/create', 'ProductsController@create')->middleware('auth');
 Route::post('/create', 'ProductsController@store')->middleware('auth');
 
-
-// Route::get('products/create', 'ProductsController@create');
-// Route::post('products/create', 'ProductsController@store');
-
-
-// Route::get('products/{id}/update', 'ProductsController@edit');
-// Route::put('products/{id}/update', 'ProductsController@update');
-
+Route::get('products/{id}/edit', 'ProductsController@edit')->middleware('auth');
+Route::patch('products/{id}update', 'ProductsController@update')->middleware('auth');
 // Route::delete('products/{id}', 'ProductsController@destroy');
-
-Route::get('/profile', 'ProfileController@showRegistrationForm2');
-
+/*--------------------------------------------------------------------------
+| CART/EVENT
+|--------------------------------------------------------------------------*/
 Route::get('/event', 'CartController@index');
 Route::post('/event', 'CartController@checkout');
 Route::post('/event/add', 'CartController@add');
