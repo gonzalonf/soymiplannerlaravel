eff94bb78909574d81064ef08f4228d63f2ba69f
Mijael Saban <mijaelsaban@gmail.com>
2017-08-09 13:55:10 -0300

    test boton mp

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -87,8 +87,14 @@
                 <h2>PREMIUM</h2><br>
                     <p>La públicacion PREMIUM aparece en las búsquedas de manera destacada.</p><br>
                     mercadolibre $477/90dias <br>
-                    <b>Nosotros: $120/90dias </b> <br><br>
-                    {{--  <strong>Premium</strong><br> --}}
+                    <b>Nosotros: $200/90dias </b> <br><br>
+                   
+
+                <a mp-mode="dftl" href="http://mpago.la/xN8O" name="MP-payButton" class='blue-ar-l-rn-none'>Suscribirme</a><br>
+                 <a style="font-size: 10px;" href="http://mpago.la/xN8O">Suscribite con MercadoPago</a><br>
+
+
+                
                     <input type='radio' style='' name='subscription' value="Paga" {{ old('subscription')=="Paga" ? 'checked='.'"'.'checked'.'"' : '' }} >
                 </div>
 
@@ -114,6 +120,23 @@
             precio.value=' ';
         }
     }
+
+</script>
+<script type="text/javascript">
+    (function() {
+        function $MPC_load() {
+            window.$MPC_loaded !== true && (function() {
+                var s = document.createElement("script");
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = document.location.protocol + "//secure.mlstatic.com/mptools/render.js";
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+                window.$MPC_loaded = true;
+            })();
+        }
+        window.$MPC_loaded !== true ? (window.attachEvent ? window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;
+    })();
 </script>
 
 </body>
